6a99ad5e05cbff6efbf20f138cf3525253628d01
Omar Chammaa <chammaa.omar@gmail.com>
2023-02-01 23:08:26 -0500

    segfaults... segfaults everywhere

--- src/clox/compiler.c
+++ src/clox/compiler.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "compiler.h"
 #include "scanner.h"
+
+#ifdef DEBUG_PRINT_CODE
+#include "debug.h"
+#endif
 
 typedef struct
 {
@@ -11,7 +16,32 @@ typedef struct
     bool panic_mode;
 } Parser;
 
+typedef enum
+{
+    PREC_NONE,
+    PREC_ASSIGNMENT, // =
+    PREC_OR,         // or
+    PREC_AND,        // and
+    PREC_EQUALITY,   // ==
+    PREC_COMPARISON, // >, >=, <, <=
+    PREC_TERM,       // + -
+    PREC_FACTOR,     // * /
+    PREC_UNARY,      // ! -
+    PREC_CALL,       // () .
+    PREC_PRIMARY     // literals and identifiers
+} Precedence;
+
+typedef void (*ParseFn)();
+
+typedef struct
+{
+    ParseFn prefix;
+    ParseFn infix;
+    Precedence precedence;
+} ParseRule;
+
 Parser parser;
+Chunk *compiling_chunk;
 
 static void error_at(Token *token, const char *message)
 {
@@ -72,9 +102,207 @@ static void consume(TokenType type, const char *message)
     error_at_current(message);
 }
 
+Chunk *current_chunk()
+{
+    return compiling_chunk;
+}
+
+static void emit_byte(uint8_t byte)
+{
+    write_chunk(current_chunk(), byte, parser.previous.line);
+}
+
+static void emit_bytes(uint8_t byte1, uint8_t byte2)
+{
+    emit_byte(byte1);
+    emit_byte(byte2);
+}
+
+static void emit_return()
+{
+    emit_byte(OP_RETURN);
+#ifdef DEBUG_PRINT_CODE
+    if (!parser.had_error)
+    {
+        disassemble_chunk(current_chunk(), "code");
+    }
+#endif
+}
+
+static void end_compiler()
+{
+    emit_return();
+}
+
+static uint8_t make_constant(Value value)
+{
+    int constant = add_constant_to_chunk(compiling_chunk, value);
+    if (constant > UINT8_MAX)
+    {
+        error("Too many constants in one chunk.");
+        return 0;
+    }
+
+    return (uint8_t)constant;
+}
+
+static void emit_constant(Value value)
+{
+    emit_bytes(OP_CONSTANT, make_constant(value));
+}
+
+static void number()
+{
+    double value = strtod(parser.previous.start, NULL);
+    emit_constant(value);
+}
+
+static ParseRule *get_rule(TokenType type);
+static void parse_precedence(Precedence precedence);
+static void expression();
+
+// binary compiles a binary infix operator (+, -, *, /) and its right-hand-side operand
+// the left-hand-side operand has already been compiled and its value has been pushed
+// onto the stack, so this function only compiles the operator and r.h.s expression
+// the expression can be any expression involing higer-predence operators, since they "bind tigher"
+static void binary()
+{
+    TokenType operator_type = parser.previous.type;
+    ParseRule *rule = get_rule(operator_type);
+    // binary operators are left-associative, so the following expression: 1 + 2 + 3 + 4
+    // should be evaluated as ((1+2) + 3) + 4
+    // so only the l.h.s operand contains the binary operator
+    // we ensure that by parsing the r.h.s expression using operators with _strictly_ higher precedence
+    // not the same precedence
+    parse_precedence((Precedence)(rule->precedence + 1));
+
+    switch (operator_type)
+    {
+    case TOKEN_PLUS:
+        emit_byte(OP_ADD);
+        break;
+    case TOKEN_MINUS:
+        emit_byte(OP_SUBTRACT);
+        break;
+    case TOKEN_STAR:
+        emit_byte(OP_MULTIPLY);
+    case TOKEN_SLASH:
+        emit_byte(OP_DIVIDE);
+    default:
+        return;
+    }
+}
+
+static void unary()
+{
+    // we've already consumed the prefix unary operator, so it's in the previous token
+    TokenType operator_type = parser.previous.type;
+
+    // compile the expression, i.e., emit its bytecode
+    // the operand appears ahead of the operator, since it already needs to be on the stack
+    // so the operator pops it, applies negation or logical negation, and pushes it back on the stack
+    parse_precedence(PREC_UNARY);
+
+    switch (operator_type)
+    {
+    case TOKEN_MINUS:
+        emit_byte(OP_NEGATE);
+        break;
+    default:
+        return;
+    }
+}
+
+static void grouping()
+{
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
+static void expression()
+{
+    parse_precedence(PREC_ASSIGNMENT);
+}
+
+ParseRule rules[] = {
+    [TOKEN_LEFT_PAREN] = {grouping, NULL, PREC_NONE},
+    [TOKEN_RIGHT_PAREN] = {NULL, NULL, PREC_NONE},
+    [TOKEN_LEFT_BRACE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_RIGHT_BRACE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_COMMA] = {NULL, NULL, PREC_NONE},
+    [TOKEN_DOT] = {NULL, NULL, PREC_NONE},
+    [TOKEN_MINUS] = {unary, binary, PREC_TERM},
+    [TOKEN_PLUS] = {NULL, binary, PREC_TERM},
+    [TOKEN_SEMICOLON] = {NULL, NULL, PREC_NONE},
+    [TOKEN_SLASH] = {NULL, binary, PREC_FACTOR},
+    [TOKEN_STAR] = {NULL, binary, PREC_FACTOR},
+    [TOKEN_BANG] = {NULL, NULL, PREC_NONE},
+    [TOKEN_BANG_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_EQUAL_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_GREATER] = {NULL, NULL, PREC_NONE},
+    [TOKEN_GREATER_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_LESS] = {NULL, NULL, PREC_NONE},
+    [TOKEN_LESS_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_IDENTIFIER] = {NULL, NULL, PREC_NONE},
+    [TOKEN_STRING] = {NULL, NULL, PREC_NONE},
+    [TOKEN_NUMBER] = {number, NULL, PREC_NONE},
+    [TOKEN_AND] = {NULL, NULL, PREC_NONE},
+    [TOKEN_CLASS] = {NULL, NULL, PREC_NONE},
+    [TOKEN_ELSE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_FALSE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_FOR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_FUN] = {NULL, NULL, PREC_NONE},
+    [TOKEN_IF] = {NULL, NULL, PREC_NONE},
+    [TOKEN_NIL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_OR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_PRINT] = {NULL, NULL, PREC_NONE},
+    [TOKEN_RETURN] = {NULL, NULL, PREC_NONE},
+    [TOKEN_SUPER] = {NULL, NULL, PREC_NONE},
+    [TOKEN_THIS] = {NULL, NULL, PREC_NONE},
+    [TOKEN_TRUE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_VAR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_WHILE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_ERROR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_EOF] = {NULL, NULL, PREC_NONE},
+};
+
+// get_rule exists solely as a layer of indirection to break a declaration cycle
+// binary cannot access the rules[] directly, since it refers to binary, so the
+// declaration cycle is broken via get_rule
+ParseRule *get_rule(TokenType type)
+{
+    return &rules[type];
+}
+
+static void parse_precedence(Precedence precedence)
+{
+    advance();
+    // parse a prefix expression
+    // all prefix precedence operators in Lox have the same precedence level
+    ParseFn prefix_rule = get_rule(parser.previous.type)->prefix;
+    if (prefix_rule == NULL)
+    {
+        error("Expect expression");
+        return;
+    }
+
+    prefix_rule();
+
+    while (precedence <= get_rule(parser.current.type)->precedence)
+    {
+        // compile tokens as long as they're of higher precedence or "bind tighter"
+        advance();
+        ParseFn infix_rule = get_rule(parser.previous.type)->infix;
+        infix_rule();
+    }
+}
+
 bool compile(const char *source, Chunk *chunk)
 {
     init_scanner(source);
+
+    compiling_chunk = chunk;
 
     parser.had_error = false;
     parser.panic_mode = false;
@@ -84,6 +312,7 @@ bool compile(const char *source, Chunk *chunk)
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    end_compiler();
 
     return !parser.had_error;
 }
